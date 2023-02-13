a3cbe312c714f14d4e6c17abe017635d3143ce83
slavik <slavik.zaliotnyi@gmail.com>
2023-02-13 22:00:20 +0200

    added adptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
+wow = new WOW({
+  boxClass: "wow", // default
+  animateClass: "animate__animated", // default
+  offset: 0, // default
+  mobile: true, // default
+  live: true, // default
+});
+wow.init();
 })
